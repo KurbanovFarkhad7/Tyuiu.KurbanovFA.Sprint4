2d33b9f706caaf94085eb0fde5480fd27a2225c5
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-03 19:37:58 +0500

    Task4

---
+++ Tyuiu.KurbanovFA.Sprint4.Task4.V9.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KurbanovFA.Sprint4.Task4.V9.Lib
+{
+    public class DataService : ISprint4Task4V9
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int result = 0;
+            for (int y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    if (matrix[y, x] % 2 == 0)
+                    {
+                        result += matrix[y, x];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.KurbanovFA.Sprint4.Task4.V9/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.KurbanovFA.Sprint4.Task4.V9.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint4.Task4.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Курбанов Ф.А. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (статический ввод)                              *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Курбанов Фархаджон Азамжанович | РППб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 8. Найдите сумму            *");
+            Console.WriteLine("* четных элементов массива                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] array = new int[5, 5];
+            Random randomValue = new Random();
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    array[y, x] = randomValue.Next(1, 8);
+                }
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int y = 0; y < array.GetLength(0); y++)
+            {
+                for (int x = 0; x < array.GetLength(1); x++)
+                {
+                    Console.Write(array[y, x] + "\t");
+                }
+                Console.WriteLine(); //Разделитель столбиков
+            }
+            Console.WriteLine("Сумма четных: " + ds.Calculate(array));
+        }
+    }
+}
