615617f11e8ab42c996b4abbe530236ebb68c246
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-03 20:13:16 +0500

    Task6

--- Tyuiu.KurbanovFA.Sprint4.Task6.V20.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint4.Task6.V20.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KurbanovFA.Sprint4.Task6.V20.Lib
     {
         public string[] Calculate(string[] array)
         {
-            string[] result = new string[array.Length];
+            string[] result = { };
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i].Length < 10)
