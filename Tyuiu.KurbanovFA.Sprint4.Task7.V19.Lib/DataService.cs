d4239c6c4051214d83f4dc960fd1553449fe51a5
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-03 21:04:12 +0500

    Task7

---
+++ Tyuiu.KurbanovFA.Sprint4.Task7.V19.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KurbanovFA.Sprint4.Task7.V19.Lib
+{
+    public class DataService : ISprint4Task7V19
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int oddCount = 0;
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < value.Length; i++)
+            {
+                int num = int.Parse(value[i].ToString());
+                matrix[i / 2, i % 2] = num;
+                if (num % 2 != 0)
+                {
+                    oddCount++;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return oddCount;
+        }
+    }
+}
---
+++ Tyuiu.KurbanovFA.Sprint4.Task7.V19/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KurbanovFA.Sprint4.Task7.V19.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint4.Task7.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Курбанов Ф.А. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Курбанов Фархаджон Азамжанович | РППб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"90817264\". Преобразуйте ее в       *");
+            Console.WriteLine("* матрицу 4 на 2 и подсчитайте количество нечетных чисел в матрице.       *");
+            Console.WriteLine("* количество отрицательных элементов.                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string input = "90817264";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Кол-во нечетных элементов в матрице: " + ds.Calculate(4, 2, input));
+        }
+    }
+}
